e306cca3082b77c38f62864ac36b30892e8f06df
Kirill Sablin <ksablin@lohika.com>
2014-09-05 16:08:14 +0300

    add p13

---
+++ src/main/scala/P13.scala
@@ -0,0 +1,9 @@
+object P13 {
+  def encodeDirect[T](lst: List[T]): List[(Int, T)] = lst.foldRight[List[(Int, T)]](List())((current, soFar) =>
+    soFar match {
+      case (count, elem) :: xs if elem == current => (count + 1, elem) :: xs
+      case Nil => List((1, current))
+      case _ => (1, current) :: soFar
+    }
+  )
+}
