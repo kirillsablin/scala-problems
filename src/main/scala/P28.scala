6ec96b106cef5b762ffac915493017f4a4d46033
Kirill Sablin <ksablin@lohika.com>
2014-09-09 09:56:20 +0300

    add p28

---
+++ src/main/scala/P28.scala
@@ -0,0 +1,10 @@
+object P28 {
+  def lsort[T](lst: List[List[T]]): List[List[T]] = lst.sortWith(_.length < _.length)
+
+  def lsortFreq[T](lst: List[List[T]]): List[List[T]] = {
+    val byLengths = lst.map(_.length).groupBy( k => k )
+    lst.sortWith( (a,b) => byLengths(a.length).length < byLengths(b.length).length)
+
+  }
+
+}
---
+++ src/test/scala/P28Spec.scala
@@ -0,0 +1,21 @@
+import org.scalatest._
+
+class P28Spec extends FlatSpec with Matchers {
+
+  import P28._
+
+  "lsort" should "sort by sub lists lengths" in {
+    lsort(List(List(1, 2, 3), List(1), List(1, 2))) should be (List(List(1), List(1, 2), List(1, 2, 3)))
+
+    lsort(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) should
+      be (List(List('o), List('d, 'e), List('d, 'e), List('m, 'n), List('a, 'b, 'c), List('f, 'g, 'h), List('i, 'j, 'k, 'l)))
+  }
+
+
+  "lsortFreq" should "sort by frequency of sub lists lengths" in {
+    lsortFreq(List(List('a, 'b, 'c), List('d, 'e), List('f, 'g, 'h), List('d, 'e), List('i, 'j, 'k, 'l), List('m, 'n), List('o))) should
+      be (List(List('i, 'j, 'k, 'l), List('o), List('a, 'b, 'c), List('f, 'g, 'h), List('d, 'e), List('d, 'e), List('m, 'n)))
+
+  }
+
+}
